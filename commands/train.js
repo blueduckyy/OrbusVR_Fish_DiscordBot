eaca04d189afaafd14ef5b0f4b9d0f1588e941c2
maggierogers <maggie.rogers@foodiker.com>
2021-03-03 11:31:19 -0600

    Started public event train

--- commands/train.js
+++ commands/train.js
@@ -1,8 +1,13 @@
 const Discord = require('discord.js');
+const fetch = require('node-fetch');
 
 
-function getServerTime() {
-    const fetch = require('node-fetch')
+async function getServerTime() {
+    const { hour, minutes } = await fetch('http://api-reborn.orbusvr.com/servertime')
+        .then(response => response.json())
+    console.log(hour);
+    console.log(minutes);
+    return hour;
 }
 
 
@@ -12,8 +17,8 @@ module.exports = {
     execute(message, args) {
         const exampleEmbed = new Discord.MessageEmbed()
             .setColor('#0099ff')
-            .setTitle('Pong')
-            .setImage('https://media1.giphy.com/media/pa5IBbv5LheEg/giphy.gif?cid=ecf05e47gi3l86xx6znrmka5ruw01j36xebz640lx5ew4k4t&rid=giphy.gif')
+            .setTitle('Public Event Train... coming soon')
+            .setImage('https://media.giphy.com/media/lGw9YbzP2vkje/giphy.gif')
 
         message.channel.send(exampleEmbed);
     },
